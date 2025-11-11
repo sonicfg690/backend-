e25c8186cd6970a217285e999fb69aeb12598b7f
Sonicfg690 <omkarmakune5@gmail.com>
2025-11-11 14:52:53 +0530

    update backend code

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get('/twitter', (req, res) =>{
 app.get('/login', (req, res) =>{
     res.send('<h1> please login at omkar makune</h1>')
 } )
-
+app.get('/youtube', (req, res) => {
+  res.send('<h3> this is youtube page</h3>!')
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
